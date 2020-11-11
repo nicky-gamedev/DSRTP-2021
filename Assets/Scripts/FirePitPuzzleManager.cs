f4d78a070c2cdcaf84a18b38681730fcbd8ab9ab
NathanMG <nathanmgaertner@gmail.com>
2020-11-11 19:07:02 -0300

    Puzzle de fogueiras criado, projeto arrumado com pastas
    Criado o puzzle de fogueirs, semelhante aos fios de Keep Talking and Nobody Explodes.
    Arrumado o projeto em pastas certinhas.

---
+++ Assets/Scripts/FirePitPuzzleManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePitPuzzleManager : MonoBehaviour
+{
+    /*
+     *  Usar o codigo de cores:
+     *  0 = Vermelho
+     *  1 = Branco
+     *  2 = Azul
+     *  3 = Amarelo
+     *  4 = Preto
+     *  5 = Rosa
+     *  6 = Verde
+     */
+    [SerializeField]
+    private Firepit[] firepits = new Firepit[6];
+    public void Interact(int position)
+    {
+        if (CheckInteraction(position, firepits))
+        {
+            Debug.LogError("Certo");
+        }
+        else
+        {
+            Debug.LogError("Errado");
+        }
+    }
+
+    private bool CheckInteraction(int pos, Firepit[] fps)
+    {
+        //Se tiver Vermelho e a interação foi feita no Top. Centro a interação foi correta
+        switch (CheckColor(pos, fps, 0, 1))
+        {
+            case 1:
+                return true;
+            case 0:
+                return false;
+            case -1:
+                //Se chegou aqui, não existia Vermelho
+                switch (CheckColor(pos, fps, 1, 3))
+                {
+                    case 1:
+                        return true;
+                    case 0:
+                        return false;
+                    case -1:
+                        //Se chegou aqui não existia nem Vermelho, nem Branco
+                        switch (CheckColor(pos, fps, 2, 2))
+                        {
+                            case 1:
+                                return true;
+                            case 0:
+                                return false;
+                            case -1:
+                                return true;
+                            default:
+                                Debug.LogError("Número retornado foi difetente do que era esperado", this);
+                                break;
+                        }
+                        break;
+                    default:
+                        Debug.LogError("Número retornado foi difetente do que era esperado", this);
+                        break;
+                }
+                break;
+            default:
+                Debug.LogError("Número retornado foi difetente do que era esperado", this);
+                break;
+        }
+        return true;
+    }
+
+    private int CheckColor(int pos, Firepit[] fps, int colorCode, int rightPos)
+    {
+        foreach (Firepit fp in fps)
+        {
+            if (fp.colorCode == colorCode)
+            {
+                if (pos == rightPos)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ Assets/Scripts/Firepit.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firepit : MonoBehaviour
+{
+    /*
+     *  Usar o codigo de cores:
+     *  0 = Vermelho
+     *  1 = Branco
+     *  2 = Azul
+     *  3 = Amarelo
+     *  4 = Preto
+     *  5 = Rosa
+     *  6 = Verde
+     */
+
+    //[Tooltip("Usar o codigo de cores: 0 = Vermelho| 1 = Branco| 2 = Azul| 3 = Amarelo| 4 = Preto| 5 = Rosa| 6 = Verde")]
+    //public int colorCode;
+    public enum enumColorCode { Vermelho, Branco, Azul, Amarelo, Preto, Rosa, Verde }
+    public enumColorCode chosenColorCode;
+    [Range(0,7)]
+    public int colorCode;
+    [Tooltip("0 = topo esquerda| 1 = topo centro| 2 = topo direita| 3 = baixo esquerda| 4 = baixo centro| 5 = baixo direita")]
+    [Range(0,5)]
+    public int position;
+    [SerializeField]
+    private FirePitPuzzleManager manager;
+    private bool listening;
+
+    private void Start()
+    {
+        switch (chosenColorCode)
+        {
+            case enumColorCode.Vermelho:
+                colorCode = 0;
+                break;
+            case enumColorCode.Branco:
+                colorCode = 1;
+                break;
+            case enumColorCode.Azul:
+                colorCode = 2;
+                break;
+            case enumColorCode.Amarelo:
+                colorCode = 3;
+                break;
+            case enumColorCode.Preto:
+                colorCode = 4;
+                break;
+            case enumColorCode.Rosa:
+                colorCode = 5;
+                break;
+            case enumColorCode.Verde:
+                colorCode = 6;
+                break;
+            default:
+                colorCode = 7;
+                break;
+        }
+        Material mt = GetComponentInChildren<MeshRenderer>().material;
+        switch (colorCode)
+        {
+            case 0:
+                mt.color = Color.red;
+                break;
+            case 1:
+                mt.color = Color.white;
+                break;
+            case 2:
+                mt.color = Color.blue;
+                break;
+            case 3:
+                mt.color = Color.yellow;
+                break;
+            case 4:
+                mt.color = Color.black;
+                break;
+            case 5:
+                mt.color = Color.magenta;
+                break;
+            case 6:
+                mt.color = Color.green;
+                break;
+            default:
+                mt.color = Color.grey;
+                break;
+        }
+        listening = false;
+    }
+    private void Update()
+    {
+        if (listening)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                manager.Interact(position);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //se o jogador entra no trigger, começa a ouvir se o input foi pressionado
+            listening = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //se o jogador sai do trigger para de ouvir
+            listening = false;
+        }
+    }
+}
