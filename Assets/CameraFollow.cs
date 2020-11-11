065ee2092027bb618c478e9dd6fb4e5a130ba705
Diego N <54142512+diegogamedev@users.noreply.github.com>
2020-11-10 23:23:54 -0300

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class CameraFollow : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] Transform target;
+
+    [Range(0,1)]
+    [SerializeField] float minPercentage;
+    [Range(0, 1)]
+    [SerializeField] float maxPercentage;
+
+    [SerializeField] RectTransform image;
+
+    [SerializeField] Vector3 lastPosToLerp;
+
+    Camera cam = null;
+    CharacterController controller;
+    CharacterControllerSimpleConfig config;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    private void Awake()
+    {
+        cam = Camera.main;
+        lastPosToLerp = transform.position;
+        controller = target.gameObject.GetComponent<CharacterController>();
+        config = target.gameObject.GetComponent<CharacterControllerSimpleConfig>();
+	}
+
+    void Update()
+	{
+        Vector3 min = new Vector3(
+            Screen.width * minPercentage,
+            Screen.height * minPercentage, 
+            cam.nearClipPlane
+            );
+
+        Vector3 max = new Vector3(
+            Screen.width * maxPercentage, 
+            Screen.height * maxPercentage, 
+            cam.nearClipPlane
+            );
+
+        Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+
+        float width = max.x - min.x;
+        float height = max.y - min.y;
+
+        image.sizeDelta = new Vector3(Mathf.Abs(width), Mathf.Abs(height));
+        image.position = min;
+
+        bool inside = screenPos.x > min.x && screenPos.x < max.x && screenPos.y > min.y && screenPos.y < max.y;
+        if (!inside)
+        {
+            lastPosToLerp = new Vector3(target.position.x, Mathf.Round(target.position.y) + 3, transform.position.z);
+        }
+
+        if (controller.isGrounded)
+        {
+            lastPosToLerp.y = config.actualFloor.position.y + 6;
+        }
+
+        Vector3 vel = new Vector3();
+        transform.position = Vector3.SmoothDamp(transform.position, lastPosToLerp, ref vel, .05f, 50f);
+    }
+    #endregion
+}
---
+++ Assets/CharacterControllerSimpleConfig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerSimpleConfig : MonoBehaviour
+{
+    public Transform actualFloor;
+
+    [SerializeField] float speed;
+    [SerializeField] float impulseJump;
+    [SerializeField] float gravity;
+
+    [SerializeField] Vector3 moveDirection;
+    [SerializeField] Vector3 verticalDirection;
+    CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveDirection = Vector3.right * x + Vector3.forward * z;
+        var look = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, look, 0.05f);
+
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            verticalDirection.y = Mathf.Sqrt(impulseJump * -2 * gravity);
+        }
+
+        if (verticalDirection.y >= gravity * 2)
+        {
+            verticalDirection.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(moveDirection * speed * Time.deltaTime);
+        controller.Move(verticalDirection * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        actualFloor = hit.gameObject.transform;
+    }
+}
