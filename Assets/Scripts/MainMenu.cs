cff5f720bc3733f3fd3e83f45eb1ead2969051e9
NathanMG <nathanmgaertner@gmail.com>
2020-11-12 16:48:06 -0300

    Main Menu, Settings, Quit
    Main Menu created, with settings, and quit confirmation.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu, optionsMenu, confirmQuit;
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        confirmQuit.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionsMenu.activeInHierarchy || confirmQuit.activeInHierarchy)
+            {
+                mainMenu.SetActive(true);
+                optionsMenu.SetActive(false);
+                confirmQuit.SetActive(false);
+            }
+            else
+            {
+                QuitGame();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        confirmQuit.SetActive(false);
+    }
+
+    public void CloseOptions()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        confirmQuit.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        confirmQuit.SetActive(true);
+    }
+
+    public void ConfirmQuit() => Application.Quit();
+
+    public void DenyQuit()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        confirmQuit.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    Resolution[] resolutions;
+    public Dropdown resDropdown;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        
+        resDropdown.ClearOptions();
+
+        int currentRes = 0;
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].height == Screen.currentResolution.height && resolutions[i].width == Screen.currentResolution.width)
+            {
+                currentRes = i;
+            }
+        }
+
+        resDropdown.AddOptions(options);
+
+        resDropdown.value = currentRes;
+        resDropdown.RefreshShownValue();
+    }
+
+    #region Volume Sliders
+    public AudioMixer audioMixer;
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetFXVolume(float volume)
+    {
+        audioMixer.SetFloat("FXVolume", volume);
+    }
+    #endregion
+
+    #region Graphics
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resIndex)
+    {
+        Screen.SetResolution(resolutions[resIndex].width, resolutions[resIndex].height, Screen.fullScreen);
+    }
+    #endregion
+}
