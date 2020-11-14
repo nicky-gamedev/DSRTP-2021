1adf42e5d38973038970f2e6c7db285c477dbf13
baveda2 <67603423+baveda2@users.noreply.github.com>
2020-11-14 01:21:21 -0300

    Revert "Merge branch 'main' of https://github.com/diegogamedev/DSRTP-2021 into main"
    This reverts commit 3a0d63e334d0d7185808ef599270b910a6625541, reversing
    changes made to 54c33255218c377479b07acdcd07f289bdbf54ed.

--- Assets/RuneManager.cs
+++ Assets/RuneManager.cs
@@ -16,80 +16,57 @@ public class RuneManager : MonoBehaviour
 
     [SerializeField] List<char> pickedRuneGroup;
     [SerializeField] List<Rune> gameRunes;
+
     [SerializeField] List<int> orderList;
 
     public bool won;
-    public int position;
-    public GameObject door;
 
+    private void Update()
+    {
+        if(orderList.Count == 4)
+        {
+            won = true;
+        }
+    }
     void OnEnable()
     {
         pickedRuneGroup = allRunes[Random.Range(0, allRunes.Count)].runes;
         gameRunes = GetComponentsInChildren<Rune>().ToList();
-        var rnd = new System.Random();
-        gameRunes = gameRunes.OrderBy(item => rnd.Next()).ToList();
 
         List<char> characterList = new List<char>();
         foreach (var item in gameRunes)
         {
-            int randomRange;
             do
             {
-                randomRange = Random.Range(0, pickedRuneGroup.Count);
+                int randomRange = Random.Range(0, gameRunes.Count);
                 item.text = pickedRuneGroup[randomRange];
+                item.order = randomRange;
             } while (characterList.Contains(item.text));
 
             characterList.Add(item.text);
-            orderList.Add(randomRange);
         }
-
-        orderList = Sort(orderList);
     }
 
     public void CheckOrder(Rune rune)
     {
-        bool error = !(rune.text == pickedRuneGroup[orderList[position]]);
+        bool error = false;
+        foreach (var item in gameRunes)
+        {
+            if (item.selected) continue;
+
+            if (rune.order < item.order) continue;
+
+            error = true;
+        }
 
         if (error)
         {
-            position = 0;
+            orderList.Clear();
             foreach (var item in gameRunes)
             {
                 item.selected = false;
             }
-            GameManager.instance.Strike();
         }
-        else position++;
-
-        if (position >= 4 && !won)
-        {
-            won = true;
-            door.SetActive(false);
-        }
-    }
-
-    List<int> Sort(List<int> data)
-    {
-        int i, j;
-        int N = data.Count;
-
-        for (j = 1; j < N; j++)
-        {
-            for (i = j; i > 0 && data[i] < data[i - 1]; i--)
-            {
-                exchange(data, i, i - 1);
-            }
-        }
-
-        return data;
-    }
-
-    void exchange(List<int> data, int m, int n)
-    {
-        int temporary;
-
-        temporary = data[m];
-        data[m] = data[n];
-        data[n] = temporary;
+        else orderList.Add(rune.order);
     }
 }
