81e070fe13c536ab2799cd5fcbfe1f46ce5737ff
Diego N <54142512+diegogamedev@users.noreply.github.com>
2020-11-13 23:19:35 -0300

    Now it's working, i promise

--- Assets/RuneManager.cs
+++ Assets/RuneManager.cs
@@ -42,6 +42,8 @@ public class RuneManager : MonoBehaviour
             characterList.Add(item.text);
             orderList.Add(randomRange);
         }
+
+        orderList = Sort(orderList);
     }
 
     public void CheckOrder(Rune rune)
@@ -64,5 +66,30 @@ public class RuneManager : MonoBehaviour
             won = true;
             door.SetActive(false);
         }
+    }
+
+    List<int> Sort(List<int> data)
+    {
+        int i, j;
+        int N = data.Count;
+
+        for (j = 1; j < N; j++)
+        {
+            for (i = j; i > 0 && data[i] < data[i - 1]; i--)
+            {
+                exchange(data, i, i - 1);
+            }
+        }
+
+        return data;
+    }
+
+    void exchange(List<int> data, int m, int n)
+    {
+        int temporary;
+
+        temporary = data[m];
+        data[m] = data[n];
+        data[n] = temporary;
     }
 }
