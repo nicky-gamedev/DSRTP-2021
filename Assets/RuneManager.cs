bbbdec67733269a6855236e75db66ccd5b2ccf39
Diego N <54142512+diegogamedev@users.noreply.github.com>
2020-11-14 12:10:27 -0300

    Revert "Now it's working, i promise"
    This reverts commit 81e070fe13c536ab2799cd5fcbfe1f46ce5737ff.

--- Assets/RuneManager.cs
+++ Assets/RuneManager.cs
@@ -50,8 +50,6 @@ public class RuneManager : MonoBehaviour
             characterList.Add(item.text);
             orderList.Add(randomRange);
         }
-
-        orderList = Sort(orderList);
     }
 
     public void CheckOrder(Rune rune)
@@ -74,30 +72,5 @@ public class RuneManager : MonoBehaviour
             won = true;
             door.SetActive(false);
         }
-    }
-
-    List<int> Sort(List<int> data)
-    {
-        int i, j;
-        int N = data.Count;
-
-        for (j = 1; j < N; j++)
-        {
-            for (i = j; i > 0 && data[i] < data[i - 1]; i--)
-            {
-                exchange(data, i, i - 1);
-            }
-        }
-
-        return data;
-    }
-
-    void exchange(List<int> data, int m, int n)
-    {
-        int temporary;
-
-        temporary = data[m];
-        data[m] = data[n];
-        data[n] = temporary;
     }
 }
